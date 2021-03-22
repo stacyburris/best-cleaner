47727f2a0cc71832273d925f77d522111c23e9ba
stacyburris <stacy1burris@gmail.com>
2021-03-22 16:50:38 -0700

    uninstalled react-page-scroll

--- src/App.js
+++ src/App.js
@@ -1,3 +1,55 @@
+import React from 'react';
+import Header from './components/header/header';
+import Carousels from './components/carousels/carousels';
+import Footer from './components/footer/footer';
+import Services from './components/services/services';
+import Recommendations from './components/recommendations/recommendations';
+import About from './components/about/about';
+import Procedure from './components/procedure/procedure';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Help from './components/help/help';
+import './App.scss';
+import './App.css';
+
+function App() {
+
+    return (
+    <>
+    <Header/>
+    <Carousels/>
+    <Help/>
+    <Recommendations/>
+    <Services/>
+    <About/>
+    <Procedure/>
+    <Footer/>
+
+    
+    {/* <Switch>
+      <Route path='/carousels' component={Carousels} exact/>
+      <Route path='/help' component={Help} exact/>
+      <Route path='/recommendations' component={Recommendations} exact/> 
+      <Route path='/services' component={Services} exact/>
+      <Route path='/about' component={About} exact/>
+      <Route path='/procedure' component={Procedure} exact/>
+   </Switch> 
+    <Route path='/footer' component={Footer} exact/> */}
+    </>
+  );  
+  
+
+}
+
+export default App;
+
+
+// package.json:
+// "react-page-scroller": "^2.1.0",
+//"lodash": "^4.17.21",
+//"prop-types": "^15.7.2",
+// "react": "^16.13.1",
+// "react-dom": "^16.13.1",
+// "react-router-dom": "^4.3.1",
 
 // REACT-PAGE-SCROLLER CODE:
 // import React from 'react';
@@ -87,46 +139,3 @@
 
 
 
-import React from 'react';
-import Header from './components/header/header';
-import Carousels from './components/carousels/carousels';
-import Footer from './components/footer/footer';
-import Services from './components/services/services';
-import Recommendations from './components/recommendations/recommendations';
-import About from './components/about/about';
-import Procedure from './components/procedure/procedure';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Help from './components/help/help';
-import { Switch, Route } from 'react-router-dom';
-import './App.scss';
-import './App.css';
-
-function App() {
-
-    return (
-    <>
-     <Header/>
-     <Carousels/>
-     <Help/>
-     <Recommendations/>
-     <Services/>
-     <About/>
-     <Procedure/>
-  <Footer/>
-    {/* <Switch>
-     
-      <Route path='/carousels' component={Carousels} exact/>
-      <Route path='/help' component={Help} exact/>
-      <Route path='/recommendations' component={Recommendations} exact/> 
-      <Route path='/services' component={Services} exact/>
-      <Route path='/about' component={About} exact/>
-      <Route path='/procedure' component={Procedure} exact/>
-   </Switch> 
-    <Route path='/footer' component={Footer} exact/> */}
-    </>
-  );  
-  
-
-}
-
-export default App;
